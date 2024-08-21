c1ac63fd033ba82149107011f3dab3aeeee513d9
mrshoffen <mrshoffen@gmail.com>
2024-08-22 00:33:08 +0300

    rework in spawn actions, add random attack and health

---
+++ src/main/java/StartingMenu.java
@@ -0,0 +1,43 @@
+import game.Simulation;
+import view.IDEAConsoleMapRenderer;
+import view.MapRenderer;
+import world.Map;
+
+public class Test
+{
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(Game.ASK_FOR_WIDTH);
+        int width = Game.readIntMatchingPattern(Game.PATTERN_FOR_WIDTH_HEIGHT_INPUT);
+
+        System.out.println(Game.ASK_FOR_HEIGHT);
+        int height = Game.readIntMatchingPattern(Game.PATTERN_FOR_WIDTH_HEIGHT_INPUT);
+
+
+        Map map = new Map(height,width);
+
+        System.out.println(Game.ASK_FOR_RENDERER_CHOICE);
+        MapRenderer renderer = Game.chooseMapRenderer();
+        renderer.setMap(map);
+
+//        Map map = new Map(12,20);
+//        MapRenderer renderer = new IDEAConsoleMapRenderer();
+//        renderer.setMap(map);
+        Simulation simulation  = new Simulation(map,renderer);
+
+        Game.runSimulationInAnotherTrhead(simulation);
+
+
+
+    }
+
+
+
+
+
+
+
+}
